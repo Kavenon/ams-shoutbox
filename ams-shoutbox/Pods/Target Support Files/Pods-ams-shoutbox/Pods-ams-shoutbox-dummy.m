1e023b33bf6d0a3707934ae2617fb390ec074c11
Kamil <kamil@Kamils-iMac.local>
2017-12-16 15:14:57 -0800

    ++

---
+++ ams-shoutbox/Pods/Target Support Files/Pods-ams-shoutbox/Pods-ams-shoutbox-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ams_shoutbox : NSObject
+@end
+@implementation PodsDummy_Pods_ams_shoutbox
+@end
--- ams-shoutbox/ams-shoutbox/ViewController.swift
+++ ams-shoutbox/ams-shoutbox/ViewController.swift
@@ -7,11 +7,60 @@
 //
 
 import UIKit
+import DGElasticPullToRefresh
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let myarray = ["item1", "item2", "item3"]
+    
+    @IBAction func onCompose(_ sender: Any) {
+        self.showAlert()
+    }
+    
+    func showAlert(){
+        let alertController = UIAlertController(title: "New message", message: "Please state your name and message", preferredStyle: .alert)
+        alertController.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Your name"
+        } )
+        alertController.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Your message"
+        } )
+        let sendAction = UIAlertAction(title: "Send", style: .default, handler: { action in
+            let name = alertController.textFields?[0].text
+            let message = alertController.textFields?[1].text
+            print(name!)
+            print(message!)
+            // do something
+        })
+        alertController.addAction(sendAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in })
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: { _ in })
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return myarray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = myarray[indexPath.item]
+        cell.detailTextLabel?.text = "test";
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let loadingView = DGElasticPullToRefreshLoadingViewCircle()
+        loadingView.tintColor = UIColor(red: 78/255.0, green: 221/255.0, blue: 200/255.0, alpha: 1.0)
+        tableView.dg_addPullToRefreshWithActionHandler({ [weak self] () -> Void in
+            // Add your logic here
+            // Do not forget to call dg_stopLoading() at the end
+            self?.tableView.dg_stopLoading()
+            }, loadingView: loadingView)
+        tableView.dg_setPullToRefreshFillColor(UIColor(red: 57/255.0, green: 67/255.0, blue: 89/255.0, alpha: 1.0))
+        tableView.dg_setPullToRefreshBackgroundColor(tableView.backgroundColor!)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
